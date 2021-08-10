d72fcc4c2bf240189fc9bf3a4d1a85c73af6e014
William Mosto <will.mosto@gmail.com>
2021-08-09 23:33:27 -0400

    commit tree 3a19e163601ac91e7acbfba10fd15b7ce404fb31

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(argument = "*") {
+    return function (adjective) {
+        return `You are ${argument}${adjective}${argument}!` 
+    }
+}
+// let empathetic = wrapAdjective('/-')("a dedicated programmer");
+// console.log(empathetic);
